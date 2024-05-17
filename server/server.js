cf2bd91c89783d0cc254fdaadbff70f70eb1499b
Mangayarkarasi.S <mangaisuresh2923@gmail.com>
2024-05-17 21:20:29 +0530

    New fixes

--- server/server.js
+++ server/server.js
@@ -1,17 +1,10 @@
-const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "data", "db.json"));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-server.use(cors());
-server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
-});
+server.listen(port);
