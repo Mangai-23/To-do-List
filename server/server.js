cb6910159365c352b87f4d32578896b1beceecad
Mangayarkarasi.S <mangaisuresh2923@gmail.com>
2024-05-17 12:40:40 +0530

    fix issues-server-port

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 3000; // Use the port provided by Render
+const PORT = process.env.PORT || 9000; // Use the port provided by Render
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
